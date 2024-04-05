e96a00d5e1c134ecff841e75466284f1df289f2c
Marvin <marvinnamwamba@gmail.com>
2024-04-05 16:06:57 +0300

    ....

--- shop/models.py
+++ shop/models.py
@@ -42,7 +42,7 @@ class review(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
     review = models.TextField(max_length=500)
-    rating = models.ForeignKey(rating, on_delete=models.PROTECT)
+    rating = models.ForeignKey(rating, on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self) -> str:
         return f'{self.review}'
@@ -85,15 +85,15 @@ class product(models.Model):
     slug = slug = slugify(name)
     price = models.PositiveIntegerField()
     discount = models.PositiveIntegerField()
-    brand = models.ForeignKey(brand, on_delete=models.PROTECT)
+    brand = models.ForeignKey(brand, on_delete=models.PROTECT, null=True, blank=True)
     reviews = models.ForeignKey(review, null=True, blank=True, on_delete=models.PROTECT)
     detail = models.TextField(max_length=3000)
     weight = models.PositiveIntegerField()
     color = models.CharField(max_length=10)
     type = models.CharField(max_length=15)
     specifications = models.TextField(max_length=2000)
-    category = models.ForeignKey(category, on_delete=models.PROTECT)
-    subcategory = models.ForeignKey(subcategory, on_delete=models.PROTECT)
+    category = models.ForeignKey(category, on_delete=models.PROTECT, null=True, blank=True)
+    subcategory = models.ForeignKey(subcategory, on_delete=models.PROTECT, null=True, blank=True)
     model = models.CharField(max_length=50)
     shop = models.ForeignKey(shop, on_delete=models.PROTECT, null=True, blank=True)
 
