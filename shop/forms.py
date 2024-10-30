4db551fc466e8bb2cbfe08ce8780fcd34fba6125
Marvin <marvinnamwamba@gmail.com>
2024-10-30 13:40:40 +0300

    add forms.py to prevent register form error

--- shop/forms.py
+++ shop/forms.py
@@ -1,5 +1,9 @@
 from django.forms import ModelForm
 from .models import public_cart, public_reviews
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class public_cart_form(ModelForm):
     class Meta:
@@ -10,3 +14,10 @@ class public_review_form(ModelForm):
     class Meta:
         model = public_reviews
         fields = ["customer_fullname", "customer_rating", "customer_feedback"]
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
